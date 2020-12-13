0efd2151a44f6b73322a1134810611165cd73421
Raed Khalaf <Raed.1142118@gmail.com>
2020-12-13 10:54:16 +0200

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+import Avatar from "components/shared/Avatar";
+
+const Header = () => (
+    <>
+        <Avatar />
+    </>
+);
+
+export default Header;
